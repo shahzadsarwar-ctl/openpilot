bb22d71dda7ba7259f3823342648d24a1247830d
shahzadsarwar-ctl <38666331+shahzadsarwar-ctl@users.noreply.github.com>
2019-08-16 08:55:20 -0600

    updated nag

--- selfdrive/controls/lib/driver_monitor.py
+++ selfdrive/controls/lib/driver_monitor.py
@@ -3,9 +3,9 @@ from common.realtime import sec_since_boot, DT_CTRL, DT_DMON
 from selfdrive.controls.lib.drive_helpers import create_event, EventTypes as ET
 from common.filter_simple import FirstOrderFilter
 
-_AWARENESS_TIME = 90.        # 1.5 minutes limit without user touching steering wheels make the car enter a terminal status
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 20.    # a first alert is issued 20s before expiration
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 5.  # a second alert is issued 5s before start decelerating the car
+_AWARENESS_TIME = 600.        # 1.5 minutes limit without user touching steering wheels make the car enter a terminal status
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 90.    # a first alert is issued 20s before expiration
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 50.  # a second alert is issued 5s before start decelerating the car
 _DISTRACTED_TIME = 10.
 _DISTRACTED_PRE_TIME_TILL_TERMINAL = 7.
 _DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 5.
